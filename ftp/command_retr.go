22d559b8ae096e621ae238bd3fde25ade14468e4
goftpd <goftpd@goftpd.org>
2020-09-20 10:59:57 +0900

    add STOR/RETR commands.

---
+++ ftp/command_retr.go
@@ -0,0 +1,68 @@
+package ftp
+
+import (
+	"context"
+	"fmt"
+	"io"
+)
+
+/*
+
+   RETRIEVE (RETR)
+
+      This command causes the server-DTP to transfer a copy of the
+      file, specified in the pathname, to the server- or user-DTP
+      at the other end of the data connection.  The status and
+      contents of the file at the server site shall be unaffected.
+*/
+
+type commandRETR struct{}
+
+func (c commandRETR) RequireState() SessionState { return SessionStateLoggedIn }
+
+func (c commandRETR) Execute(ctx context.Context, s *Session, params []string) error {
+	if len(params) == 0 {
+		return s.ReplyStatus(StatusSyntaxError)
+	}
+
+	if s.data == nil {
+		return s.ReplyStatus(StatusCantOpenDataConnection)
+	}
+
+	if s.dataProtected {
+		if err := s.ReplyWithMessage(StatusTransferStatusOK, "Opening connection for download using TLS/SSL."); err != nil {
+			return err
+		}
+	} else {
+		if err := s.ReplyWithMessage(StatusTransferStatusOK, "Opening connection for download."); err != nil {
+			return err
+		}
+	}
+	defer s.data.Close()
+	defer func() {
+		s.data = nil
+	}()
+
+	path := s.server.fs.Join(s.currentDir, params)
+
+	user, ok := s.User()
+	if !ok {
+		return s.ReplyStatus(StatusNotLoggedIn)
+	}
+
+	reader, err := s.server.fs.DownloadFile(path, user)
+	if err != nil {
+		return s.ReplyError(StatusActionNotOK, err)
+	}
+
+	n, err := io.Copy(s.data, reader)
+	if err != nil {
+		return s.ReplyError(StatusActionNotOK, err)
+	}
+
+	return s.ReplyWithMessage(StatusDataClosedOK, fmt.Sprintf("OK, received %d bytes.", n))
+}
+
+func init() {
+	commandMap["RETR"] = &commandRETR{}
+}
