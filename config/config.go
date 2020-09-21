528e831538fd37bd116951f0a751020ed05a52f5
goftpd <goftpd@goftpd.org>
2020-09-21 15:45:48 +0900

    split out the reflection part of ParseServerOpts so its reusable

--- config/config.go
+++ config/config.go
@@ -4,7 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 	"strings"
+
+	"github.com/pkg/errors"
 )
 
 type Namespace string
@@ -84,4 +88,67 @@ func ParseFile(file string) (*Config, error) {
 	}
 
 	return &c, nil
+}
+
+// parse searches opts for any fields tagged with "goftpd" and then attempts
+// to parse and insert the corresponding Line
+func (c *Config) parse(lines []Line, opts interface{}) error {
+	rv := reflect.ValueOf(opts)
+
+	for _, l := range lines {
+		fields := strings.Fields(l.text)
+
+		if len(fields) < 2 {
+			return errors.Errorf("error on line %d", l.line)
+		}
+
+		tag := strings.ToLower(fields[0])
+
+		// iterate over the opts
+		for i := 0; i < reflect.Indirect(rv).NumField(); i++ {
+
+			if value, ok := reflect.Indirect(rv).Type().Field(i).Tag.Lookup("goftpd"); ok {
+				if value == tag {
+					if reflect.Indirect(rv).Field(i).CanSet() {
+						switch reflect.Indirect(rv).Field(i).Kind() {
+
+						case reflect.String:
+							reflect.Indirect(rv).Field(i).SetString(strings.Join(fields[1:], " "))
+
+						case reflect.Int:
+							if len(fields) > 2 {
+								return errors.Errorf("error parsing int on line %d: too many fields", l.line)
+							}
+
+							num, err := strconv.Atoi(fields[1])
+							if err != nil {
+								return errors.Errorf("error parsing int on line %d: not a number", l.line)
+							}
+
+							reflect.Indirect(rv).Field(i).SetInt(int64(num))
+
+						case reflect.Slice:
+							switch reflect.Indirect(rv).Field(i).Type().Elem().Kind() {
+							case reflect.Int:
+
+								var nums []int
+
+								for _, f := range fields[1:] {
+									num, err := strconv.Atoi(f)
+									if err != nil {
+										return errors.Errorf("error parsing int on line %d: not a number", l.line)
+									}
+									nums = append(nums, num)
+								}
+
+								reflect.Indirect(rv).Field(i).Set(reflect.ValueOf(nums))
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return nil
 }
--- config/server.go
+++ config/server.go
@@ -1,10 +1,6 @@
 package config
 
 import (
-	"reflect"
-	"strconv"
-	"strings"
-
 	"github.com/goftpd/goftpd/ftp"
 	"github.com/pkg/errors"
 )
@@ -12,67 +8,15 @@ import (
 func (c *Config) ParseServerOpts() (*ftp.ServerOpts, error) {
 	var opts ftp.ServerOpts
 
-	if _, ok := c.lines[NamespaceServer]; !ok {
+	lines, ok := c.lines[NamespaceServer]
+	if !ok {
 		return nil, errors.New("no server options provided")
 	}
 
-	rv := reflect.ValueOf(&opts)
-
-	for _, l := range c.lines[NamespaceServer] {
-		fields := strings.Fields(l.text)
-
-		if len(fields) < 2 {
-			return nil, errors.Errorf("error on line %d", l.line)
-		}
-
-		tag := strings.ToLower(fields[0])
-
-		// iterate over the opts
-		for i := 0; i < reflect.Indirect(rv).NumField(); i++ {
-
-			if value, ok := reflect.Indirect(rv).Type().Field(i).Tag.Lookup("goftpd"); ok {
-				if value == tag {
-					if reflect.Indirect(rv).Field(i).CanSet() {
-						switch reflect.Indirect(rv).Field(i).Kind() {
-
-						case reflect.String:
-							reflect.Indirect(rv).Field(i).SetString(strings.Join(fields[1:], " "))
-
-						case reflect.Int:
-							if len(fields) > 2 {
-								return nil, errors.Errorf("error parsing int on line %d: too many fields", l.line)
-							}
-
-							num, err := strconv.Atoi(fields[1])
-							if err != nil {
-								return nil, errors.Errorf("error parsing int on line %d: not a number", l.line)
-							}
-
-							reflect.Indirect(rv).Field(i).SetInt(int64(num))
-
-						case reflect.Slice:
-							switch reflect.Indirect(rv).Field(i).Type().Elem().Kind() {
-							case reflect.Int:
-
-								var nums []int
-
-								for _, f := range fields[1:] {
-									num, err := strconv.Atoi(f)
-									if err != nil {
-										return nil, errors.Errorf("error parsing int on line %d: not a number", l.line)
-									}
-									nums = append(nums, num)
-								}
-
-								reflect.Indirect(rv).Field(i).Set(reflect.ValueOf(nums))
-
-							}
-						}
-					}
-				}
-			}
-		}
+	if err := c.parse(lines, &opts); err != nil {
+		return nil, err
 	}
 
 	return &opts, nil
+
 }
